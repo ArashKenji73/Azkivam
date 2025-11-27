4f7b283352641c63f6f66bb3470d325103b43ad5
ArashKenji73 <kenjiarash@gmail.com>
2025-11-27 11:32:24 +0330

    fix : watcher goes to prodlist

--- app/components/ProductList.vue
+++ app/components/ProductList.vue
@@ -1,38 +1,38 @@
 <template>
-  <div>
-    <div class="grid grid-cols-4">
-      <!-- SSR skeleton -->
-      <div v-if="loading" v-for="i in 8" :key="i" class="h-60 bg-gray-200 animate-pulse rounded"></div>
+  <div class="grid grid-cols-4">
+    <!-- SSR skeleton -->
+    <div v-if="loading" v-for="i in 8" :key="i" class="h-60 bg-gray-200 animate-pulse rounded"></div>
 
-      <!-- Product list -->
-      <template v-else>
-        <template v-for="product in products">
-          <single-product :data="product" />
-        </template>
+    <!-- Product list -->
+    <template v-else>
+      <template v-for="product in products">
+        <single-product :data="product" />
       </template>
-    </div>
+    </template>
+  </div>
 
-    <!-- Load more -->
-    <div v-if="products" class="mt-6 text-center" >
-      <button
-        v-if="products.length < totalItems"
-        @click="fetchProducts"
-        class="px-6 py-3 bg-black text-white rounded hover:bg-gray-700"
-        :disabled="loading"
-      >
-      
-        {{ loading ? 'Loading…' : 'Show More' }}
-      </button>
+  <!-- Load more -->
+  <div v-if="products" class="mt-6 text-center">
+    <button v-if="products.length < totalItems" @click="fetchProducts"
+      class="px-6 py-3 bg-black text-white rounded hover:bg-gray-700" :disabled="loading">
 
-      <!-- <p v-else class="text-gray-500 mt-4">No more products</p> -->
-    </div>
+      {{ loading ? 'Loading…' : 'Show More' }}
+    </button>
 
-
-
+    <!-- <p v-else class="text-gray-500 mt-4">No more products</p> -->
   </div>
 </template>
 
 <script setup>
+const route = useRoute()
 import { useProducts } from '~/composable/useProducts';
 const { products, fetchProducts, loading, totalItems } = useProducts()
+
+
+watch(
+  () => route.query.merchants,
+  (newVal, oldVal) => {
+    fetchProducts(true)
+  },{ immediate: true }
+)
 </script>
